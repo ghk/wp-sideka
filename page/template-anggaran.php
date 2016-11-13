2e8bf3e43a9c4273ad2c0e727a67ddd85b8ef6f5
Gozali Kumara <ghk@microvac.co.id>
2016-11-13 21:01:11 +0700

    bar chart text

--- page/template-anggaran.php
+++ page/template-anggaran.php
@@ -111,6 +111,12 @@ $package_exists = json_decode($json)->success;
             font-family: 'Open Sans', Helvetica, Arial, sans-serif;
         }
 
+        .bar-values {
+            fill: #000;
+            fill-opacity: 1;
+            stroke: none;
+        }
+
     </style>
 
     <div class="clearfix">
@@ -213,7 +219,6 @@ $package_exists = json_decode($json)->success;
                     .showControls(false)   //Allow user to switch between 'Grouped' and 'Stacked' mode.
                     .groupSpacing(0.1)    //Distance between each group of bars.
                 ;
-            window.chart = chart;
 
             //chart.bars.forceY([0]);
             chart.yAxis
@@ -224,6 +229,51 @@ $package_exists = json_decode($json)->success;
             d3.select(id+' svg')
                 .datum(transformed)
                 .call(chart);
+
+            var oldUpdate = chart.update;
+            function doUpdate() {
+                oldUpdate.apply(this, arguments);
+                var container = d3.select(id + ' svg');
+                container.selectAll('.nv-multibar .nv-group').each(function(group){
+                    var g = d3.select(this);
+
+                    // Remove previous labels if there is any
+                    g.selectAll('text').remove();
+                    g.selectAll('.nv-bar').each(function(bar){
+                        var b = d3.select(this);
+                        var barWidth = b.attr('width');
+                        var barHeight = b.attr('height');
+                        if(bar.y < 100000000){
+                            return;
+                        }
+
+                        g.append('text')
+                            // Transforms shift the origin point then the x and y of the bar
+                            // is altered by this transform. In order to align the labels
+                            // we need to apply this transform to those.
+                            .attr('transform', b.attr('transform'))
+                            .text(function(d){
+                                // Two decimals format
+                                var height = this.getBBox().height;
+                                console.log(bar);
+                                return d.key + " " + parseFloat(bar.y).toFixed(2);
+                            })
+                            .attr('y', function(){
+                                // Center label vertically
+                                var height = this.getBBox().height;
+                                return parseFloat(b.attr('y')) - 10; // 10 is the label's magin from the bar
+                            })
+                            .attr('x', function(){
+                                // Center label horizontally
+                                var width = this.getBBox().width;
+                                return parseFloat(b.attr('x')) + (parseFloat(barWidth) / 2) - (width / 2);
+                            })
+                            .attr('class', 'bar-values');
+                    });
+                });
+            }
+            chart.update = doUpdate;
+            chart.update();
 
             nv.utils.windowResize(chart.update);
 
